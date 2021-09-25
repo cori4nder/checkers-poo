32a04d6562455ade18b766447724266f6eb0684d
laplace1123 <lgd@academico.ufpb.br>
2021-09-24 22:50:02 -0300

    Checkers v1.0 | POO - Java

---
+++ src/main/java/com/laplace/checkers/Main.java
@@ -0,0 +1,8 @@
+package com.laplace.checkers;
+import com.laplace.checkers.JanelaPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        new JanelaPrincipal();
+    }
+}
